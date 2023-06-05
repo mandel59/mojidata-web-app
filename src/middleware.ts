6492dfe5a7633845c5b3d92178c43f6b4ccce321
Ryusei Yamaguchi <mandel59@gmail.com>
2023-06-05 19:17:37 +0900

    Remove debug line

--- src/middleware.ts
+++ src/middleware.ts
@@ -5,7 +5,6 @@ export function middleware(request: NextRequest): NextResponse | undefined {
   const { pathname } = request.nextUrl
   if (pathname.startsWith('/mojidata/')) {
     // redirect /mojidata/U+6F22 to /mojidata/漢
-    console.log(pathname)
     const m = pathname.match(/^\/mojidata\/[uU](?:\+|%2B)?([0-9a-fA-F]{4,6})$/)
     if (m) {
       const codePoint = m[1]
