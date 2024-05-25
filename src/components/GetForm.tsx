77578489d49da063ff87987bb0fbff4c9b4e16b2
Ryusei Yamaguchi <mandel59@gmail.com>
2024-05-25 22:43:55 +0900

    Fix GetForm component

--- src/components/GetForm.tsx
+++ src/components/GetForm.tsx
@@ -19,12 +19,13 @@ const GetForm = forwardRef<HTMLFormElement, FormProps>(function GetForm(
           const form = e.target as HTMLFormElement
           const submitter = (e.nativeEvent as SubmitEvent)
             .submitter as HTMLButtonElement | null
-          const formMethod = submitter?.formMethod ?? form.method
-          const formAction = submitter?.formAction ?? form.action
+          const formMethod = submitter?.formMethod || form.method
+          const formAction = submitter?.formAction || form.action
           if (formMethod === 'get') {
             e.preventDefault()
             e.stopPropagation()
             const url = new URL(formAction)
+            url.search = ''
             const formData = new FormData(form)
             formData.forEach((value, key) => {
               if (typeof value === 'string') {
